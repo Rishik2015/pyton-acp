9a174bc13831f2b9115a8ef496089fe58ce17aba
Rishik2015 <panchalminu@gmail.com>
2025-08-02 09:57:33 +0530

    initial  commit

---
+++ firstpyacp.py
@@ -0,0 +1,6 @@
+name= 'rishik'
+grade= 5
+ispass= True
+print(name,"is in grade",grade)
+print("my name is",name)
+print("did rishik pass?",ispass)
---
+++ turtes.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.begin_fill
+turtle.pencolor("black")
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.end_fill
+
+turtle.done()
