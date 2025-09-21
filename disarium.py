f48c7ed8016199bc49a53a0ec2a989614e57fe51
Rishik2015 <panchalminu@gmail.com>
2025-09-21 10:14:03 +0530

    initial commit

---
+++ disarium.py
@@ -0,0 +1,19 @@
+# How to Check if a Number is a Disarium Number
+# Find the number of digits: in the given number. 
+# Iterate through each digit: from left to right. 
+# Raise each digit: to the power of its positional value (1st digit to the power of 1, 2nd digit to the power of 2, etc.). 
+# Sum: these results. 
+# If the sum equals: the original number, then it is a Disarium number
+
+total_amount = int(input("enter your total number :"))
+number_of_digits = [2,3,4,5,6,7,8,9]
+1 digit = power_of_2:
+2 digit = power_of_3:
+3 digit = power_of_4:
+4 digit = power_of_5:
+5 digit = power_of_6:
+6 digit = power_of_7:
+7 digit = power_of_9:
+8 digit = power_of_10:
+
+add these digits
