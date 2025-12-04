9a6926ada72966a4c4c96f018cabdcf782ed5572
Rishik2015 <panchalminu@gmail.com>
2025-12-04 21:28:17 +0530

    initial commit

---
+++ quiz.py
@@ -0,0 +1,6 @@
+def question :
+print("welcome to cricket quiz competition")
+print ("Question: who is chiku?")
+print("OPTION 1  virat kholi,sachin,dhoni")
+if answer : virat kholi
+print(" you win ")
