82775b4226af22e2f74222fbf03975714b89c42c
Rishik2015 <panchalminu@gmail.com>
2025-12-04 22:30:41 +0530

    initial commit

---
+++ dice.py
@@ -0,0 +1,14 @@
+
+
+def generate() :
+    print("enter a number")
+if numberentered==thenumber
+     print("the entered is  eqivalent to the number")
+
+else 
+     print("it is not equivalent")
+
+
+
+
+   
