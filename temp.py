8d44ef8045ed0a79b8d45493608bce6c4490d110
Rishik2015 <panchalminu@gmail.com>
2026-01-31 09:10:07 +0530

    initial commit

--- temp.py
+++ temp.py
@@ -1,6 +1,6 @@
-s=float(input("enter your temperature"))
+summer=float(input("enter your temperature"))
 
-if s <= 50:
+if summer <= 50.6:
     print("it is appropriate to wear light clothes")
 else:
     print("it is not appropriate to wear light clothes")    
