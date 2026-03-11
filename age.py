f02d595b74dc63444c1c26b3458bdb5886bf3f11
Rishik2015 <panchalminu@gmail.com>
2026-03-11 11:46:05 +0530

    initial commit

---
+++ age.py
@@ -0,0 +1,7 @@
+birthyear=int(input("enter your birth year  "))
+currentyear= 2026
+age=currentyear-birthyear
+print("your age is ",age)
+
+if (age>20):
+      print ("you are eligible for mr rajs class")
