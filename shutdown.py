e5dc02747262f7aea7302632db9f72442dc9b044
Rishik2015 <panchalminu@gmail.com>
2026-03-14 10:43:52 +0530

    initial commit

--- shutdown.py
+++ shutdown.py
@@ -1,8 +1,10 @@
-user=input("enter your answer y/n")
+
 def shutdown(user):
     if user=='y':
         print("shutting down")
-    elif user == 'n'  :
+    elif user == 'n':
        print("abort shutting down")  
     else:
-        print("sorry")   
+        print("sorry")
+user=input("enter your answer y/n")
+shutdown(user)           
