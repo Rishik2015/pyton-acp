29969a32fb62ca9b015234b41f0cb30418d1aee9
Rishik2015 <panchalminu@gmail.com>
2026-04-11 10:00:55 +0530

    initial commit

---
+++ listcomprehension.py
@@ -0,0 +1,13 @@
+# user = int(input("enter a number "))
+# li1 = [1,2,3,4,5,user] 
+# print(li1)
+# li2 = [1,3,5,7,9]
+
+
+fruits = ['apples','mangoes','oranges']
+cfruits = ['A','M','O']
+
+
+mapped = zip(fruits, cfruits)
+
+print(list(mapped))
