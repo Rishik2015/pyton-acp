e451fa32d995d1f30c63dd41b4696fab94c0f125
Rishik2015 <panchalminu@gmail.com>
2026-07-11 09:09:14 +0530

    initial commit

---
+++ len.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root = Tk()
+root.title("todayer i will show yoer home in italianito de ")
+root.configure(bg = "light blue")
+root.geometry("600x500")
+def lenght () :
+    l = 23 iinches
---
+++ ssprites.py
@@ -0,0 +1,21 @@
+import math
+import random
+import pygame
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 500
+PLAYER_START_X = 370
+PLAYER_START_Y = 380
+ENEMY_START_Y_MIN = 50
+ENEMY_START_Y_MAX = 150
+ENEMY_SPEED_X = 4
+ENEMY_SPEED_Y = 40
+BULLET_SPEED_Y = 10
+COLLISION_DISTANCE = 27
+
+
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+textX = 10
+textY = 10
+
