a8d794e55930fc85b26886d5a2ddfdd7cf853957
Rishik2015 <panchalminu@gmail.com>
2026-07-11 09:26:19 +0530

    initial commit

--- len.py
+++ len.py
@@ -1,7 +1,18 @@
-from tkinter import *
-root = Tk()
-root.title("todayer i will show yoer home in italianito de ")
-root.configure(bg = "light blue")
-root.geometry("600x500")
-def lenght () :
-    l = 23 iinches
+import tkinter as tk
+
+def get_name():
+    typed_name = name_entry.get()
+    print("Hello,", typed_name + "!")
+
+root = tk.Tk()
+root.title("enter a measurement in inchees")
+
+name_entry = tk.Entry(root)
+name_entry.pack()
+
+my_button = tk.Button(root, text="Say Hello", command=get_name)
+my_button.pack()
+
+typed_name = typed_name // 2.54
+
+root.mainloop()
